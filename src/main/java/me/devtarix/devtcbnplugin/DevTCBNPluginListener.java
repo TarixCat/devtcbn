b363eea33a0a86e077264972ae5eb2fcbb9995b5
TarixCat <mrcatter05@gmail.com>
2020-05-12 09:31:37 +1000

    Kick Notification!

--- src/main/java/me/devtarix/devtcbnplugin/DevTCBNPluginListener.java
+++ src/main/java/me/devtarix/devtcbnplugin/DevTCBNPluginListener.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 
 public class DevTCBNPluginListener implements Listener {
 
@@ -11,5 +12,10 @@ public class DevTCBNPluginListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         String playerName = event.getPlayer().getDisplayName();
             event.getPlayer().sendMessage(ChatColor.GOLD + "Welcome " + playerName + " to the server.");
+    }
+
+    @EventHandler
+    public void onKick(PlayerKickEvent event) {
+        DevTCBNPlugin.broadcastMessage("User " + event.getPlayer() + "has just been kicked for: " + event.getReason());
     }
 }
